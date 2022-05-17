0bdfb388d6f721e5c325f0b0a0fbfae08e97f812
eungbing <104728315+eungbing22@users.noreply.github.com>
2022-05-17 22:47:35 +0900

    Update ScoreInterface.java

--- db/ScoreInterface.java
+++ db/ScoreInterface.java
@@ -1,10 +1,4 @@
-//0429_오후 미션_1
-// 오후 4시 40분
-
-// 여러개 같이 사용중
-// DBConn
-// StudentInterface, StudentDao, StudentVo, StudentInput, StudentModify, StudentDelete;
-// ScoreInterface, ScoreDao, ScoreVo, ScoreInput, ScoreModify, ScoreDelete;
+//0429_오후
 
 package db;
 
@@ -13,11 +7,10 @@ import java.util.List;
 public interface ScoreInterface {
 	public boolean insert(ScoreVo vo);
 	public boolean update(ScoreVo vo);
-	public boolean delete(int sno); //int sno로 다시 바꿈//id꺼를 다 지우면 안되니께
+	public boolean delete(int sno); 
 	public List<ScoreVo> select(String findStr);
 	public ScoreVo selectOne(int sno);
-	//public ScoreVo selectOne(String id);
-	public String search(String id); //추가하고 대체
+	public String search(String id);
 }
 
 
