98d47285e7ca7a8f89b244925a12452eaa5127b4
eungbing <104728315+eungbing22@users.noreply.github.com>
2022-05-17 22:39:33 +0900

    Update DBConn.java

--- db/DBConn.java
+++ db/DBConn.java
@@ -1,11 +1,10 @@
 //0429_3
-//오전 11시20분
 
 //학생관리 조회,수정삭제,조회
 //성적관리 조회,수정삭제,조회에서 다 쓸 수 있게
-//공통적으로 작업한다심
+//공통적으로 작업
 
-// 여러개 같이 사용중
+// 같이 사용중
 // DBConn
 // StudentInterface, StudentDao, StudentVo, StudentInput, StudentModify, StudentDelete;
 // ScoreInterface, ScoreDao, ScoreVo, ScoreInput, ScoreModify, ScoreDelete;
@@ -23,8 +22,7 @@ public class DBConn {
 		private String user = "hong";
 		private String pwd = "1111";
 	
-		//디폴트로 만듦 
-		//나와같은 패키지안의 클래스들한테만 공유하겠다
+		// 디폴트로 만들어 나와 같은 패키지안의 클래스에게만 공유
 		Connection conn;
 		
 		//생성자
