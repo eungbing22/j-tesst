919f0ed6aa113cfe67bafda7f70c83ec0a18dec3
eungbing <104728315+eungbing22@users.noreply.github.com>
2022-05-17 23:06:12 +0900

    Update ProductInterface.java

--- db.product/ProductInterface.java
+++ db.product/ProductInterface.java
@@ -11,11 +11,9 @@ public interface ProductInterface {
 	public boolean delete(int sno);
 	
 	public List<ProductVo> select(String findStr);
-	//검색어를 매개변수로 전달받아 그 결과를 list의 <ProductVo>로 만들어 반환하겠다.
 
 	public ProductVo selectOne(int sno);
-	//프라이머리키라 필요하다!
-	
+
 	public ProductVo selectOne(String code);
-	//코드를 매개변수로 전달받아 그 결과를 vo로 만들어 반환하겠다.
+
 }
